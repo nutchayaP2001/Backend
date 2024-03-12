63be5ffc5e7af0b1794bcb8fc153ca76e8afa0f2
nutchayaP2001 <nutchaya.sehun@gmail.com>
2024-03-13 03:38:15 +0700

    ok2

--- index.js
+++ index.js
@@ -25,12 +25,12 @@ dotenv.config();
 
 const app = express();
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE 
-})
+// const connection = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE 
+// })
 
 const sessionStore = SequelizeStore(session.Store);
 
